1d81ed07f65e21fcc1332157c82f2f39f208a170
Calum J. Eadie <calum@calumjeadie.com>
2016-05-15 22:53:29 +0100

    Removing BOM and \r line endings.

--- osx-installer/build-osx-installer.sh
+++ osx-installer/build-osx-installer.sh
@@ -15,7 +15,13 @@ mkdir build
 cp -r osx-installer/InstallMedicalSpellChecker build/
 
 # OS X requires a UFT-8 dictionary.
-iconv -f UTF-16LE -t UTF-8 US-English-Medical-Dictionary.dic > build/InstallMedicalSpellChecker/scripts/dictionary
+# Convert dos to unix.
+# Remove \r line endings.
+# Remove BOM.
+iconv -f UTF-16LE -t UTF-8 US-English-Medical-Dictionary.dic \
+	| tr -d '\r' \
+	| awk 'NR==1{sub(/^\xef\xbb\xbf/,"")}{print}' \
+	> build/InstallMedicalSpellChecker/scripts/dictionary
 
 chmod a+x build/InstallMedicalSpellChecker/scripts/postinstall
 
