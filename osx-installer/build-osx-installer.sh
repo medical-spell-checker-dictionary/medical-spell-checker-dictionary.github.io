1676a055043066915ba882589cfeb0945847b386
Calum J. Eadie <calum@calumjeadie.com>
2016-05-15 10:53:48 +0100

    Added rough osx installer.

---
+++ osx-installer/build-osx-installer.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+
+readonly prog_name=$(basename $0)
+readonly prog_dir=$(dirname $0)
+readonly args="$@"
+
+cd $prog_dir/..
+
+rm -rf build
+mkdir build
+
+cp -r osx-installer/InstallMedicalSpellChecker build/
+cp US-English-Medical-Dictionary.dic build/InstallMedicalSpellChecker/scripts/dictionary
+
+chmod a+x build/InstallMedicalSpellChecker/scripts/postinstall
+
+pkgbuild \
+	--nopayload \
+	--scripts build/InstallMedicalSpellChecker/scripts \
+	--identifier com.calumjeadie.medical-spell-checker \
+	'build/Install Medical Spell Checker.pkg'
