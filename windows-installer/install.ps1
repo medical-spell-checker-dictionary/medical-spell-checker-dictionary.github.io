e036f05bf4cf6d2a357cb5462969536a26ab67a7
Devon Buchanan <devon@divinenephron.co.uk>
2016-05-21 12:02:22 +0100

    Renaming dictionary shrinking script.

--- windows-installer/install.ps1
+++
@@ -1,6 +0,0 @@
-﻿
-$sourcePath = "..\US-English-Medical-Dictionary.dic"
-
-$content = Get-Content $sourcePath 
-
-$content | select -First 44502 | Out-File ($sourcePath.Replace(".dic",".min.dic"))
---
+++ windows-installer/shrinkdictionary.ps1
@@ -0,0 +1,9 @@
+﻿
+param(
+	[Parameter(Mandatory=$true)][string]$from,
+	[Parameter(Mandatory=$true)][string]$to
+)
+
+$content = Get-Content $from
+
+$content | select -First 44502 | Out-File ($to)
